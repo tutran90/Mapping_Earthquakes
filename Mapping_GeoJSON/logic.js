e950822c80875e52540091cd1696e84afd8c5b72
tutran90 <thuytutran90@gmail.com>
2021-12-04 13:53:49 -0600

    pushing the torontoNeighborhood.js file

--- Mapping_GeoJSON/logic.js
+++ Mapping_GeoJSON/logic.js
@@ -105,16 +105,17 @@ let sanFranAirport =
 // Create the map object with center and zoom level.
 //let map = L.map('mapid').setView([30, 30], 2); --> standard way to set up the map with .setView
 
-//Creating a title layer with LL quickstart doc. using tileLayer() method:
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/outdoors-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: API_KEY
-});
+////Creating a title layer with LL quickstart doc. using tileLayer() method:
+//// We create the tile layer that will be the background of our map.
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox/outdoors-v11',
+//     tileSize: 512,
+//     zoomOffset: -1,
+//     accessToken: API_KEY
+// });
+
 // // Then we add our 'graymap' tile layer to the map.
 // streets.addTo(map); --> this would be used if you only had when tile layer 
 
@@ -126,19 +127,33 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
+
+// 13.5.5 Map GeoJSON LineStrings
+
+// Creating a light view and replacing the streets view
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/light-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
-  Dark: dark
+  //Street: streets,
+  Dark: dark,
+  Light: light
 };
 
 
 //13.5.4 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [30, 30],
+  center: [44.0, -80.0],
   zoom: 2,
-  layers: [streets]
+  layers: [dark]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -147,21 +162,42 @@ L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
 // want to access the LARGE dataset after the tileLayer method to ensure the map loads 
-let airportData = "https://raw.githubusercontent.com/tutran90/Mapping_Earthquakes/main/Mapping_GeoJSON/majorAirports.json"
+let airportData = "https://raw.githubusercontent.com/tutran90/Mapping_Earthquakes/main/Mapping_GeoJSON/majorAirports.json";
+
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/tutran90/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
+
+
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}; 
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
   console.log(data);
 
 // Creating a GeoJSON layer with the retrieved data.
 // Skill drill add a popup marker that displays all airports' codes and names
-L.geoJSON(data, {
-  onEachFeature: function(feature, layer){
-    console.log(layer);
-    layer.bindPopup("<h2>" + "Aiport code:" + " " + feature.properties.faa + "</h2> <hr> <h3>" +
-    "Airport name:" + " " + feature.properties.name)
-  }
-}).addTo(map);
+// L.geoJSON(data, {
+//   onEachFeature: function(feature, layer){
+//     console.log(layer);
+//     layer.bindPopup("<h2>" + "Aiport code:" + " " + feature.properties.faa + "</h2> <hr> <h3>" +
+//     "Airport name:" + " " + feature.properties.name)
+//   }
+// }).addTo(map);
+// }); --> this block of code was for airport data 
+
+ // Creating a GeoJSON layer with the retrieved data.
+ L.geoJSON(data, {
+   style: myStyle,
+   onEachFeature: function(feature, layer){
+     console.log(layer);
+     layer.bindPopup("<h2>" + "Airline" + " " + feature.properties.src + "</h2> <hr> <h3>" + 
+     "Destination" + " " + feature.properties.dst)
+   }
+ }).addTo(map);
 });
 
 
